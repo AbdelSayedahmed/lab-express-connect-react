a499d48254ab67d769830371b72fb84e37540f28
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-07-03 18:04:50 -0400

    minor changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <nav className="nav-container">
       <Link to="/logs"><span>Logs</span></Link>
-      <Link to="/new"><span>New Log</span></Link>
+      <Link to="/logs/new"><span>New Log</span></Link>
       <Link to="/"><span>Home</span></Link>
     </nav>
   );
