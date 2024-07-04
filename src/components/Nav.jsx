52a4485216f444dd5f810b515838899fb0a5b78f
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-07-03 20:15:54 -0400

    nav css added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <nav className="nav-container">
+      <Link to="/"><span>Home</span></Link>
       <Link to="/logs"><span>Logs</span></Link>
       <Link to="/logs/new"><span>New Log</span></Link>
-      <Link to="/"><span>Home</span></Link>
     </nav>
   );
 }
